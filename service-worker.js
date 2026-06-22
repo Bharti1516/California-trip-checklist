f48ee5c2c83e3504666ff53342205959e532058b
Bharti1516 <siraskarbharti16@gmail.com>
2026-06-22 11:44:27 -0700

    Refresh dashboard offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,12 @@
-const CACHE="california-packing-v3";
+const CACHE="california-packing-v4";
 const FILES=["./","index.html","manifest.webmanifest","app-icon.svg"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match("index.html")))));
+self.addEventListener("fetch",event=>{
+  if(event.request.mode==="navigate"){
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put("index.html",copy));return response}).catch(()=>caches.match("index.html")));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+});
 
