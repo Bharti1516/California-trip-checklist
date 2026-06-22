6146662a12acf4686b0478d0c0b5d61ad867d6ae
Bharti1516 <siraskarbharti16@gmail.com>
2026-06-22 11:24:27 -0700

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+const CACHE="california-packing-v2";
+const FILES=["./","index.html","manifest.webmanifest","app-icon.svg"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match("index.html")))));
+
